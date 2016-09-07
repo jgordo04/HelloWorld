c07460ab1b620fae2f79eaf9afe713ffe0edcc72
jamiegordonlipkin <jamie.gordonlipkin@gmail.com>
2016-09-06 22:04:24 -0400

    Added an input.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package com.example.helloworld;
+import java.util.*;
 
 /**
  * Created by jamiegordonlipkin on 9/6/16.
@@ -6,5 +7,9 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is HelloWorld in IntelliJ!!");
+        Scanner stdin = new Scanner(System.in);
+        String name = stdin.nextLine();
+
+        System.out.println("Hello " + name);
     }
 }
