f9c4d02dfd7e1fbd0a8e35572aef712c2c4bdddc
Jamie Gordon-Lipkin <jamiegordonlipkin@Jamies-MacBook-Pro.local>
2016-09-06 21:56:13 -0400

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by jamiegordonlipkin on 9/6/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is HelloWorld in IntelliJ!!");
+    }
+}
